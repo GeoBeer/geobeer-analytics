57896388c339eb4438c2b5a958bdc30b49d217f7
rastrau <ralph.straumann@gmail.com>
2019-03-01 19:44:25 +0100

    Update 1-visualize-swissgis-gender-distribution.R

--- 1-visualize-swissgis-gender-distribution.R
+++ 1-visualize-swissgis-gender-distribution.R
@@ -16,6 +16,7 @@ if (!require(readr)) {
 }
 source(here('functions.R'))
 
+
 # Ingest data ------------------------------------------------------------------
 
 # Read and combine the (preprocessed) data on the #SwissGIS Twitter list
